13c5082312bb8bb767a1b0aaaa1dccb8a77cc7f2
Weslley Silva <weslley.the@gmail.com>
2023-11-29 08:49:20 -0300

    first commit

---
+++ Api_Spring/src/test/java/com/unipe/Api_Spring/ApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipe.Api_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
